e9be67f3d56a54781bca39563823350c72709cbe
weisteck <hugo.celdran@gmail.com>
2021-06-17 12:50:50 +0200

    conf laravel to set vuejs front on serve

--- back/routes/web.php
+++ back/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ViewController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/{any}', [ViewController::class, 'app'])
+    ->where('any','^(?!api).*$');
